1913570f249285e6b57dde517dcb8fb3fb43a7f6
Vladyslav Polovynka <polovynka.dev@gmail.com>
2021-12-31 18:53:48 +0200

    added sidebar toggle

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+
+const toggleMenu = (nav, active) => {
+
+	nav.classList.toggle(active);
+};
+
+
+const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
+
+	const burgerBtn = document.querySelector(openBtn);
+	const navigation = document.querySelector(menu);
+	const naviClose = document.querySelectorAll(closeTrigger);
+	console.log('naviClose: ', naviClose);
+
+	burgerBtn.addEventListener('click', () => {
+
+		toggleMenu(navigation, classActiveMenu)
+	});
+
+	naviClose.forEach(item => {
+
+		item.addEventListener('click', () => {
+
+			toggleMenu(navigation, classActiveMenu);
+		});
+	});
+
+
+};
+
+export default slideMenu;
